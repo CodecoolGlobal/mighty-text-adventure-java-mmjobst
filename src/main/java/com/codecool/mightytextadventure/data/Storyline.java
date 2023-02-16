a91b02c13d1453cac95b4cdff19789dd2efa0e0c
HugeIdeaMixPlenty <Juliacodecoolmeet@gmail.com>
2023-02-16 23:18:25 +0100

    played around more and more

--- src/main/java/com/codecool/mightytextadventure/data/Storyline.java
+++ src/main/java/com/codecool/mightytextadventure/data/Storyline.java
@@ -4,27 +4,32 @@ public enum Storyline {
     HILFE ("""
             Es ist immer gut nach Hilfe zu fragen! Hier die wichtigsten Eingaben für dich: 
             INNOLAB - Du vermisst die gute alte Zeit im Prog Basic? Geh zurück ins INNOLAB!
-            REDEN - Du hast genug davon einsam und alleine zu programmieren oder 
-            möchtest den weisen Rat eines Mentors hören? Dann gilt für dich "REDEN ist Gold, schweigen ist langweilig!"
-            UMSCHAUEN - Du möchtest deine Umgebung besser kennenlernen, dann hilft UMSCHAUEN. Es ist immer gut zuerst einmal seine Möglichkeiten zu kennen, 
+            REDEN - Du hast genug davon einsam und alleine zu programmieren oder möchtest den weisen Rat eines Mentors hören? 
+            Dann gilt für dich "REDEN ist Gold, schweigen ist langweilig!"
+            UMSCHAUEN - Du möchtest deine Umgebung besser kennenlernen, dann hilft UMSCHAUEN. 
+            Es ist immer gut zuerst einmal seine Möglichkeiten zu kennen, 
             GEHEN - 10.000 Schritte am Tag ist dein Ziel? Dann starte deine Reise und geh zu den Dingen die dich interessieren!
             """),
     HELLO("""
             Wie heißt du?
             """),
     AUFWACHEN("""
-            , du befindest dich in einem großen Raum mit Glasfront, zwar weißt du nicht wie du hierhergekommen bist 
-            (vielleicht war der letzte Wodka Wellness im Respawn gestern doch zuviel?), 
+            , du befindest dich in einem großen Raum mit Glasfront, zwar weißt du nicht wie du hierhergekommen bist, 
             aber du bist jetzt wohl bei CodeCool inskribiert... plötzlich schwebt dir das Wort "INNOLAB" vor, 
-            sollst du es wagen? Oder besser um HILFE fragen? """),
+            sollst du es wagen oder besser um HILFE fragen? """),
 
     INNOLAB("""
-            Du bist im Inno-Lab des Talent Gardens, das Prog Basic Modul läuft schon, was willst du als nächstes tun? 
-            Dich UMSCHAUEN, mit den anderen Personen im Raum REDEN oder HINAUF in den zweiten Stock gehen?"""),
+            Du bist im Inno-Lab des Talent Gardens, das Prog Basic Modul läuft schon seit einiger Zeit, was willst du 
+            als Nächstes tun? Dich UMSCHAUEN, mit den anderen Personen im Raum REDEN oder HINAUF in den zweiten Stock gehen?
+            """),
 
     UMSCHAUEN("""
             Um dich herum sind andere Menschen, außerdem siehst du ein Sofa, ein paar Schreibtische und Sessel.
             Vor dir steht ein Notebook. Möchtest du mit jemandem REDEN oder zu etwas GEHEN?
+            """),
+    GEHEN("""
+            Möchtest du zu dem bequem aussehenden SOFA oder dem aufgedrehten BEAMER gehen? Du kannst auch das NOTEBOOK aufklappen,
+             oder in den zweiten Stock HINAUF gehen. 
             """),
 
     REDEN("""
@@ -51,7 +56,95 @@ public enum Storyline {
             Hindurchgehen musst du alleine."
              \n
             Möchtest du noch mit jemand anderem REDEN oder lieber zurück ins INNOLAB?
+            """),
+    SOFA(""" 
+            Oh...mhm...das ist so bequem... und du bist sooo müde ...  du könntest für 5 Minuten, nur ganz kurz, 
+            nur ein Nickerchen...\n
+            
+            Oh no, besser schnell wieder ans NOTEBOOK setzen!!
+            """),
+    NOTEBOOK("""
+            Die SI-Week hat gestartet und du sitzt zuhause vor deinem Notebook, was machst du?\n
+            
+            -> du öffnest JOURNEY
+            -> du wirfst das Notebook aus dem FENSTER
+            -> du legst dich ins BETT
+            """),
+    OVER("""
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁
+            ⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀
+            ⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀
+            ⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀
+            ⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀
+            ⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀
+            ⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆
+            ⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀
+            ⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀
+            ⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀
+            ⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀
+            ⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            """),
+    JOURNEY("""
+            Du starrst die Journey Projekte an und nöchtest mit einem starten, aber warte - welcher Wochentag ist heute?\n
+                    
+            ANFANG der Woche 
+            oder
+            ENDE der Woche
+            """),
+    ANFANG("""
+            Schau auf Discord, Manuela hat dort mittwochs eine Liste der Fehler gepostet die sie beim durchcoden der Beispiele gefunden hat. 
+            Jetzt kannst auch du mit dem Coden loslegen bis es nächste Woche wieder ins INNOLAB geht!
+            """),
+    ENDE("""
+            Oh no, es ist zu früh in der Woche! Du kannst noch nicht mit den Beispielen beginnen, 
+            denn Manuela muss sie erst durchcoden, um euch dann eine Liste der Fehler zu schicken...
+                        
+            Aber willst du es trotzdem versuchen und ein Journey PROJEKT starten?\n
+            Oder besser WARTEN?
+            """),
+    WARTEN("""
+            Dank Manuelas Hilfe schaffst du es bis Montag einen Großteil der Journey Projekte zu lösen. Good for you, 
+            das wird ein toller SI-Week Review im INNOLAB!
+            """),
+    PROJEKT("""
+            Stunden später und keine Zeile Code weiter glaubst du ein teuflisches Lachen zu hören... ganz leise... 
+            jedesmal wenn du das Journey-Projekt öffnest...
+            ⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⣠⡾⠛⠛⠛⠛⠛⠛⠛⠛⠛⠻⠿⠿⠷⠶⠶⠶⠶⣶⡀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⢰⡿⠀⠀⠸⣷⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠸⣧⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠸⣧⠀⠀⠀⢻⡆⠀⠀⢻⣦⡤⠀⠀⠀⠀⠀⣀⣾⡇⠀⢹⣇⠀⠀⠀⠀⠀⠀
+            ⠀⠀⢿⡄⠀⠀⠈⣿⡀⠀⠀⠙⠿⣶⡤⠀⠀⢠⣾⠟⠁⠀⠀⢻⡆⠀⠀⠀⠀⠀
+            ⠀⠀⠸⡇⠀⠀⠀⢹⣇⠀⠀⡀⠀⠀⠀⠀⠀⠀⠁⢀⠀⠀⡄⠈⢿⡄⠀⠀⠀⠀
+            ⠀⠀⠀⢿⠀⠀⠀⠀⢿⡄⠀⢹⣷⣄⡀⣶⣦⡀⢀⣿⣆⣾⡇⠀⠘⣷⠀⠀⠀⠀
+            ⠀⠀⠀⠘⡇⠀⠀⠀⠸⣷⠀⠀⢻⡿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠸⣆⠀⠀⠀
+            ⠀⠀⠀⠀⣿⡀⠀⠀⠀⢻⡆⠀⠀⠀⠈⢻⣿⠃⠙⢿⡏⠈⠇⠀⠀⠀⢹⡄⠀⠀
+            ⠀⠀⠀⠀⢸⣇⠀⠀⠀⠈⠉⠀⠀⠀⠀⠀⠁⠀⠀⠈⠀⠀⠀⠀⠀⠀⠈⣿⡀⠀
+            ⠀⠀⠀⠀⠀⣿⣀⣴⠾⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠿⠿⠿⠷⠶⠶⠶⠾⠷⠀
+            ⠀⠀⠀⠀⠀⢸⡿⠁⣴⣾⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⠶⠂⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠁⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀
+            
+            Nach weiteren Stunden liegst du weinend am Boden, vielleicht doch besser WARTEN?!
+            """),
+    BEAMER("""
+            Oh no, es ist anscheinend Freitag und das heißt Demo-Days der Beamer ist bereits an und die ersten Präsentationen laufen, 
+            Panik steigt in dir hoch. Was wirst du präsentieren? 
+            
+            -> nichts, du läufst hinaus und kommst NIE wieder
+            -> du schmuggelst dich zu einem anderen TEAM
+            -> du legst dich aufs SOFA und hoffst, dass dich niemand bemerkt
+            """),
+    TEAM("""
+            """),
+
+    HINAUF("""
+
             """);
+
+
 
     private final String text;
 
--- src/main/java/com/codecool/mightytextadventure/logic/Game.java
+++ src/main/java/com/codecool/mightytextadventure/logic/Game.java
@@ -24,36 +24,56 @@ public class Game {
         }
     }
 
+    int knowledge = 0;
+
     private boolean step() {
 //        display.printMessage(Storyline.HELLO.getText());
         String inputFromUser = input.getInputFromUser().toUpperCase();
 //        if display.printMessage(inputFromUser + ", " + Storyline.AUFWACHEN.getText());
 //        inputFromUser = input.getInputFromUser().toUpperCase();
-        if(inputFromUser.equals(Storyline.REDEN.toString())) {
+        if (inputFromUser.equals(Storyline.REDEN.toString())) {
             display.printMessage(Storyline.REDEN.getText());
+            knowledge += 1;
         }
-              if(inputFromUser.equals(Storyline.MARTIN.toString())) {
+
+        public class storyElement(String StorylineElement){
+
+                    if (inputFromUser.equals(Storyline.MARTIN.toString())) {
+                display.printMessage(Storyline.MARTIN.getText());
+                knowledge += 1;
+            }
+        }
+        if (inputFromUser.equals(Storyline.MARTIN.toString())) {
             display.printMessage(Storyline.MARTIN.getText());
+            knowledge += 1;
         }
-              if(inputFromUser.equals(Storyline.EMAD.toString())) {
+        if (inputFromUser.equals(Storyline.EMAD.toString())) {
             display.printMessage(Storyline.EMAD.getText());
+            knowledge += 1;
         }
-              if(inputFromUser.equals(Storyline.SELBST.toString())) {
+        if (inputFromUser.equals(Storyline.SELBST.toString())) {
             display.printMessage(Storyline.SELBST.getText());
         }
-              if(inputFromUser.equals(Storyline.MITSTUDIERENDE.toString())) {
+        if (inputFromUser.equals(Storyline.MITSTUDIERENDE.toString())) {
             display.printMessage(Storyline.MITSTUDIERENDE.getText());
         }
-              if(inputFromUser.equals(Storyline.UMSCHAUEN.toString())) {
+        if (inputFromUser.equals(Storyline.UMSCHAUEN.toString())) {
             display.printMessage(Storyline.UMSCHAUEN.getText());
         }
-        if(inputFromUser.equals(Storyline.INNOLAB.toString())) {
+        if (inputFromUser.equals(Storyline.INNOLAB.toString())) {
+            display.printMessage(Storyline.INNOLAB.getText());
+        }
+        if (inputFromUser.equals("FENSTER") || inputFromUser.equals("NIE")) {
+            display.printMessage(Storyline.OVER.getText());
+            return false;
+        }
+        if (inputFromUser.equals("SOFA") || inputFromUser.equals("BETT")) {
+            display.printMessage(Storyline.SOFA.getText());
+        }
+        if (inputFromUser.equals(Storyline.INNOLAB.toString())) {
             display.printMessage(Storyline.INNOLAB.getText());
         }
 
-        
-        
-        
         return true;
     }
 }
