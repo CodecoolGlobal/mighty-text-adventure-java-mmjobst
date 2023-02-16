e105283e35c75d91b667966286f4509a036d403c
tentacletea <c98.william@gmail.com>
2023-02-16 13:28:53 +0100

    add enums, add switch action validator

--- src/main/java/com/codecool/mightytextadventure/data/Storyline.java
+++ src/main/java/com/codecool/mightytextadventure/data/Storyline.java
@@ -11,7 +11,28 @@ public enum Storyline {
 
     UMSCHAUEN("""
             Du schaust dich um, es gibt Tische, Stühle ... 
-            """)
+            """),
+    REDEN("""
+            Im Raum befinden sich MARTIN, EMAD, MITSTUDIERENDE und du SELBST. Mit wem möchtest du reden?\s
+            """),
+    EMAD("""
+            "Every mistake you make is progress! Growth Mindset!"\n
+            Möchtest du noch mit jemand anderem REDEN oder lieber zurück ins INNOLAB?
+            """),
+    MARTIN("""
+            "Ich versuche, deinen Verstand zu befreien, Neo. Aber ich kann dir nur die Tür zeigen. 
+            Hindurchgehen musst du alleine."\n
+            Möchtest du noch mit jemand anderem REDEN oder lieber zurück ins INNOLAB?
+            """),
+    MITSTUDIERENDE("""
+            "Hallo, I bin da Fleischi, woas is dei Lieblingsessen - vielleicht koch i es amal für di!"\n\n
+            Möchtest du noch mit jemand anderem REDEN oder lieber zurück ins INNOLAB?
+            """),
+    SELBST("""
+            "Was tue ich eigentlich hier? Was ist das Wesen von Freiheit? Bin ich wirklich frei - 
+            wie kann ich wissen ob meine Entscheidungen wirklich meine eigenen sind?"\n
+            Möchtest du noch mit jemand anderem REDEN oder lieber zurück ins INNOLAB?
+               """),
     ;
     private final String text;
 
@@ -19,7 +40,7 @@ public enum Storyline {
         this.text = text;
     }
 
-    public String getText(){
+    public String getText() {
         return text;
     }
 }
--- src/main/java/com/codecool/mightytextadventure/logic/Game.java
+++ src/main/java/com/codecool/mightytextadventure/logic/Game.java
@@ -1,10 +1,13 @@
 package com.codecool.mightytextadventure.logic;
 
 import com.codecool.mightytextadventure.data.Level;
+import com.codecool.mightytextadventure.data.StoryCard;
 import com.codecool.mightytextadventure.data.Storyline;
 import com.codecool.mightytextadventure.data.Actions;
 import com.codecool.mightytextadventure.ui.Display;
 import com.codecool.mightytextadventure.ui.Input;
+
+import java.util.SortedMap;
 
 public class Game {
     private final Level[] levels;
@@ -32,8 +35,22 @@ public class Game {
         actionFromUser = actionFromUser.toUpperCase();
 
         ActionManager actionManager = new ActionManager();
-        boolean b = actionManager.actionValidator(actionFromUser);
-        System.out.println("boolean: " + b);
+        StoryCard storyCard = new StoryCard();
+
+        boolean validBool = actionManager.actionValidator(actionFromUser);
+        System.out.println("boolean: " + validBool);
+
+        if (validBool) {
+            switch (Actions.valueOf(actionFromUser)) {
+                case UMSCHAUEN:
+                    return storyCard.lookAround();
+                case GEHEN:
+                    return storyCard.goTo();
+                case REDEN:
+                    return storyCard.talkTo();
+            }
+        }
+
 
         return true;
     }
