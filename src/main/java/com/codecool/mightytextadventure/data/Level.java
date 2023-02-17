7df650cdd279d04e43a65b74727795340765a991
HugeIdeaMixPlenty <Juliacodecoolmeet@gmail.com>
2023-02-17 03:24:43 +0100

    pls be there again!!!!!

---
+++ src/main/java/com/codecool/mightytextadventure/data/Level.java
@@ -0,0 +1,8 @@
+package com.codecool.mightytextadventure.data;
+
+public class Level {
+    private String description;
+    public Level(String description) {
+        this.description = description;
+    }
+}
